7fc983e9da218b08bc3cc48bcd2b463dc5bb61ea
D13G05 <mydiego-99@hotmail.com>
2022-11-11 15:57:02 -0500

    prueba distribuidos

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
       
       <header className="App-header">
       <h1>Este es un titulo de prueba 2</h1>
+      <h1>Este es un titulo de prueba PRODUCCION</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
