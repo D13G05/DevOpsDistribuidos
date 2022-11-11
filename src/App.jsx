9f6cbfd1f17489beabb434cc844dbb74f1cb6421
D13G05 <mydiego-99@hotmail.com>
2022-11-11 11:23:39 -0500

    Prueba 2

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       
       <header className="App-header">
-      <h1>Este es un titulo de prueba</h1>
+      <h1>Este es un titulo de prueba 2</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
